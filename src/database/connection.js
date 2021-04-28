fe584cc446124ae914c61e4b0d70254ea9df5e26
Yoiber Beitar <yoiberrenteria2017@gmail.com>
2021-04-28 15:47:23 -0500

    database

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,11 +1,11 @@
 import {Pool} from 'pg'
 
+const { Pool } = require('pg');
 const pool = new Pool({
-  user: 'zefupquyblpcnc',
-  host: 'ec2-54-224-120-186.compute-1.amazonaws.com',
-  database: 'ddhqnap6lldd4e',
-  password: '6a76417bf29d4fcb2f754226a0765a2bbcf44e85d8aad36b3831e1f3db9efe3e',
-  port: 5432,
-})
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 export default pool
