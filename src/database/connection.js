9308d475af003b1fb0cf41cdc2e1366a69018859
Yoiber Beitar <yoiberrenteria2017@gmail.com>
2021-04-28 15:11:55 -0500

    database

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,10 +1,10 @@
 import {Pool} from 'pg'
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'pruebaApiRest',
-  password: '1001032661',
+  user: 'zefupquyblpcnc',
+  host: 'ec2-54-224-120-186.compute-1.amazonaws.com',
+  database: 'ddhqnap6lldd4e',
+  password: '6a76417bf29d4fcb2f754226a0765a2bbcf44e85d8aad36b3831e1f3db9efe3e',
   port: 5432,
 })
 
